b6a7e229879c909d23e1ef3e11a9ebf77d2e7afe
Yusif Salam-zade <yusalam@gmail.com>
2020-10-12 17:17:33 +0300

    1.07

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,23 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.port || 3003
+
 const randomHash = Math.random().toString(36)
+let hashText = ''
 
 const getHashNow = () => {
-  console.log(new Date().toISOString() + ' ' + randomHash)
+  hashText = new Date().toISOString() + ' ' + randomHash
+  console.log(hashText)
   setTimeout(getHashNow, 5000)
 }
 
 getHashNow()
+
+app.get('/', (_req, res) => {
+  res.send(hashText)
+})
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`)
+})
