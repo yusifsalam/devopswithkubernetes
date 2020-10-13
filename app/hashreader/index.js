4eefb27c5d44890863c0fee01899df51710799e1
Yusif Salam-zade <yusalam@gmail.com>
2020-10-13 16:39:36 +0300

    1.11

--- app/hashreader/index.js
+++ app/hashreader/index.js
@@ -2,15 +2,23 @@ const path = require('path')
 const fs = require('fs')
 
 const directory = path.join('/', 'usr', 'src', 'app', 'files')
-const filePath = path.join(directory, 'hash.txt')
+const hashFilePath = path.join(directory, 'hash.txt')
+const pingFilePath = path.join(directory, 'ping.txt')
 
 let hashText = ''
 
 const getFile = async () => {
-  await fs.readFile(filePath, (err, data) => {
+  await fs.readFile(hashFilePath, (err, data) => {
     if (err) return console.log('FAILED TO READ FILE', '----------------', err)
     hashText = data.toString()
     console.log('Current hash ', hashText)
+  })
+
+  await fs.readFile(pingFilePath, (err, data) => {
+    if (err) {
+      return console.log('failed to read ping file ', err)
+    }
+    console.log('Ping / Pongs: ', data.toString())
   })
 
   setTimeout(getFile, 5000)
