a56deb2fa04a07954c09ccc42d449b5e19d2b102
Yusif Salam-zade <yusalam@gmail.com>
2020-10-15 14:34:59 +0300

    1.12

--- project/index.ts
+++ project/index.ts
@@ -1,9 +1,42 @@
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
+import axios from 'axios'
+
 const app = express()
 const port = process.env.PORT || 3000
 
+const directory = path.join('/', 'usr', 'src', 'app', 'files')
+let filePath = ''
+
+const fileAlreadyExists = async () =>
+  new Promise((res) => {
+    fs.stat(filePath, (err, stats) => {
+      if (err || !stats) return res(false)
+      return res(true)
+    })
+  })
+
+const getImage = async () => {
+  const todayString = new Date().toISOString().split('T')[0]
+  filePath = path.join(directory, `image_${todayString}.jpg`)
+  if (await fileAlreadyExists()) return
+
+  await new Promise((res) => fs.mkdir(directory, (err) => res()))
+  const response = await axios.get('https://picsum.photos/1200', {
+    responseType: 'stream',
+  })
+  console.log('saving file ', filePath)
+  response.data.pipe(fs.createWriteStream(filePath))
+}
+
 app.get('/', (_req, res) => {
   res.send('Hello world!')
+})
+
+app.get('/randomImage', async (_req, res) => {
+  await getImage()
+  res.sendFile(filePath)
 })
 
 app.listen(port, () => {
